716234ccb40572209976eb4130e9733d61de5604
Emmanuel Hernández Maqueda <emhermaq@gmail.com>
2021-06-19 19:02:05 -0500

    Utilizando useEffect para hacer una peticion al usar la pagina de 'Nosotros'

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Nosotros = () => {
+
+    // const datos = [
+    //     {id: 1, nombre: 'React'},
+    //     {id: 2, nombre: 'Angular'},
+    //     {id: 3, nombre: 'Vue js'}
+    // ]
+
+    const [equipo, setEquipo] = useState([])
+
+
+    React.useEffect(() => {
+
+        // console.log('UseEffect')
+        // setEquipo(datos)
+
+        obtenerDatos()
+    }, [])
+
+    // Petición a JSON Placeholder
+    const obtenerDatos = async () => {
+        const data = await fetch('https://jsonplaceholder.typicode.com/users')
+        const users = await data.json()
+        // console.log(users)
+        setEquipo(users)
+    }
+
     return (
         <div>
             <h1>Nosotros</h1>
+            <ul>
+                {
+                    equipo.map( (item) => (
+                        <li key={item.id}> {item.name} - {item.email} </li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
